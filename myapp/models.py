8212a224ccd4086c0ff0ad0d647b18872a6cbf12
Yicheng Lu <lu16a@uwindsor.ca>
2022-06-06 16:34:11 -0400

    Initial commit

---
+++ myapp/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+import datetime
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    topic = models.ForeignKey(Topic, related_name='courses', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    for_everyone = models.BooleanField(default=True)
+    description = models.TextField(max_length=300, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Student(User):
+    CITY_CHOICES = [('WS', 'Windsor'),
+                    ('CG', 'Calgery'),
+                    ('MR', 'Montreal'),
+                    ('VC', 'Vancouver')]
+    school = models.CharField(max_length=50, blank=True)
+    address = models.CharField(max_length=50, blank=True, )
+    city = models.CharField(max_length=2, choices=CITY_CHOICES, default='WS')
+    interested_in = models.ManyToManyField(Topic)
+
+    def __str__(self):
+        return self.username
+
+class Order(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='orders')
+    levels = models.PositiveIntegerField()
+    ORDER_CHOICES = [(0, 'Cancelled'), (1, 'Order Confirmed')]
+    order_status = models.IntegerField(choices=ORDER_CHOICES, default=1)
+    order_date = models.DateField()
+
+    @classmethod
+    def total_cost(self):
+        return self.course.price
+
+    def __str__(self):
+        return self.course.name + " by " + self.student.username
