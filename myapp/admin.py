22bd61fb0298dca0f3111e5c8e1472041ce1282a
Yihao Yang <yyh64549@outlook.com>
2022-08-11 22:45:51 -0400

    Features 1 and 2

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,34 @@
+import decimal
+
 from django.contrib import admin
 from django.db import models
 from .models import Topic, Course, Student, Order
 
+
 # Register your models here.
-admin.site.register(Topic)
-admin.site.register(Course)
+class CourseInline(admin.TabularInline):
+    model = Course
+    extra = 0
+
+
+class TopicAdmin(admin.ModelAdmin):
+    inlines = [CourseInline]
+    fields = ['name']
+
+
+class CourseAdmin(admin.ModelAdmin):
+    actions = ['course_discount']
+    list_display = ['name', 'topic', 'price']
+    ordering = ['topic', 'name']
+
+    @admin.action(description='10%% discount')
+    def course_discount(self, request, queryset):
+        for course in queryset:
+            course.price *= decimal.Decimal(0.9)
+            course.save()
+
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Student)
 admin.site.register(Order)
